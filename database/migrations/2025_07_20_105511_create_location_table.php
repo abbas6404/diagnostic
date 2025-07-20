f08c9566879c279135e3bae01b4ef960de5a7473
abbas6404 <aiolimited123@gmail.com>
2025-07-20 17:02:19 +0600

    location table added

---
+++ database/migrations/2025_07_20_105511_create_location_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('location', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->enum('type', ['division', 'district', 'upazila'])->index();
+            $table->unsignedBigInteger('district_id')->index()->nullable(); 
+            $table->unsignedBigInteger('division_id')->index()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('location');
+    }
+};
